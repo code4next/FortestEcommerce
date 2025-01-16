8bc5ba117592171bed48f2f62a69ea2a3e7003fc
Saiful Alom <yoursaiful41@gmail.com>
2025-01-16 19:49:45 +0600

    defult cart update

--- data/cart.js
+++ data/cart.js
@@ -3,19 +3,19 @@ import { deliveryOptions } from "./deleveryOptions.js";
 export let cartItems = JSON.parse(localStorage.getItem('cartItems')) || 
 [
   {
-    productId: undefined,
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity : 2,
     deliveryOptionId: '1'
 
   },
   {
-    productId: undefined,
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
     quantity : 5,
     deliveryOptionId: '2'
 
   },
   {
-    productId: undefined,
+    productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
     quantity : 6,
     deliveryOptionId: '3'
 
@@ -41,6 +41,7 @@ export function addTocart(productId){
     cartItems.push({
       productId,
       quantity: 1,
+      deliveryOptionId: '1'
      
     });
   }
