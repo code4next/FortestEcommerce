7a5742efd356feb5c6d40a8124ac50762529a916
Saiful Alom <yoursaiful41@gmail.com>
2025-01-18 08:18:15 +0600

    test with jasmin

---
+++ jasmin-tests/moneyTest.js
@@ -0,0 +1,17 @@
+import {formateCurency } from '../script/utils/money.js';
+
+describe('Test sute: testMoney',() =>{
+    it('convert cents into dolar',() => { 
+        expect(formateCurency(2000)).toEqual('20.00')
+    })
+
+    it('work with 0',() =>{
+        expect(formateCurency(0)).toEqual('0.00')
+    })
+
+    it (' work with round' ,()=>{
+        expect(formateCurency(2000.5)).toEqual('20.01')
+    })
+
+
+})
